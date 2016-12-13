33ba39591fa0f9f503b8ef6c02304f0de81731dc
Jan Bator <jan.bator@xing.com>
2016-12-13 11:59:28 +0100

    Comment out points.

--- inception.js
+++ inception.js
@@ -4,18 +4,18 @@ function init() {
 
 class Inception {
   constructor() {
-    this.dropField            = document.getElementById("dropField");
-    this.dropField.ondrop     = this.dropHandler.bind(this);
-    this.dropField.ondragover = this.dragoverHandler.bind(this);
-
+    this.dropField    = document.getElementById("dropField");
     this.displayField = document.getElementById("displayField");
-    this.points       = document.getElementsByClassName("point");
+    this.outerImage   = document.getElementById("outerImage");
+    // this.points       = document.getElementsByClassName("point");
 
-    this.points[0].onmousedown = this.dragPointHandler.bind(this);
-    this.points[0].onmouseup   = this.dropPointHandler.bind(this);
+    this.dropField.addEventListener("drop", this.dropHandler.bind(this));
+    this.dropField.addEventListener("dragover", this.dragoverHandler.bind(this));
 
-    this.outerImage        = document.getElementById("outerImage");
-    this.outerImage.onload = this.imageLoadHandler.bind(this);
+    this.outerImage.addEventListener("load", this.imageLoadHandler.bind(this));
+
+    // this.points[0].addEventListener("drag", this.dragPointHandler.bind(this));
+    // this.points[0].addEventListener("dragend", this.dropPointHandler.bind(this));
   }
 
   dropHandler(event) {
@@ -40,40 +40,38 @@ class Inception {
     this.outerImage.style.top  = (this.displayField.clientHeight - this.outerImage.clientHeight) / 2;
     this.outerImage.style.left = (this.displayField.clientWidth - this.outerImage.clientWidth) / 2;
 
-    this.resetPointPositions();
+    // this.resetPointPositions();
   }
 
-  resetPointPositions() {
-    var middle = {
-      x: this.displayField.clientWidth / 2,
-      y: this.displayField.clientHeight / 2
-    }
-    var offset = 20
+  // resetPointPositions() {
+  //   var middle = {
+  //     x: this.displayField.clientWidth / 2,
+  //     y: this.displayField.clientHeight / 2
+  //   }
+  //   var offset = 20
+  //
+  //   this.points[0].style.top  = middle.y - offset;
+  //   this.points[0].style.left = middle.x - offset;
+  //
+  //   this.points[1].style.top  = middle.y + offset;
+  //   this.points[1].style.left = middle.x - offset;
+  //
+  //   this.points[2].style.top  = middle.y + offset;
+  //   this.points[2].style.left = middle.x + offset;
+  //
+  //   this.points[3].style.top  = middle.y - offset;
+  //   this.points[3].style.left = middle.x + offset;
+  // }
 
-    this.points[0].style.top  = middle.y - offset;
-    this.points[0].style.left = middle.x - offset;
+  // dragPointHandler(event) {
+  //   event.preventDefault();
+  //   this.points[0].classList.add("hidden");
+  // }
 
-    this.points[1].style.top  = middle.y + offset;
-    this.points[1].style.left = middle.x - offset;
-
-    this.points[2].style.top  = middle.y + offset;
-    this.points[2].style.left = middle.x + offset;
-
-    this.points[3].style.top  = middle.y - offset;
-    this.points[3].style.left = middle.x + offset;
-  }
-
-  dragPointHandler(event) {
-    event.preventDefault();
-    console.debug('drag');
-    this.points[0].style.top  = event.clientY;
-    this.points[0].style.left = event.clientX;
-  }
-
-  dropPointHandler(event) {
-    event.preventDefault();
-    console.debug('drop');
-    this.points[0].style.top  = event.clientY;
-    this.points[0].style.left = event.clientX;
-  }
+  // dropPointHandler(event) {
+  //   event.preventDefault();
+  //   this.points[0].classList.remove("hidden");
+  //   this.points[0].style.top  = event.clientY - 5;
+  //   this.points[0].style.left = event.clientX - 5;
+  // }
 }
